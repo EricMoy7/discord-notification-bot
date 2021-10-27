206e62e587a7fcd00d9af3996fd78da2876ef6b5
Eric <ericzmoy@gmail.com>
2021-10-27 18:10:16 -0500

    table successfully returned

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium_modules import get_table_data
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -22,22 +24,16 @@ client = discord.Client()
 async def test():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    address = os.getenv("ONE_ADDRESS")
+    one_address = os.getenv("ONE_ADDRESS")
 
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(f"https://kingdom.watch/personal/{address}")
+    driver.get(f"https://kingdom.watch/personal/{one_address}")
 
     WebDriverWait(driver, 10).until(
         EC.invisibility_of_element_located((By.CLASS_NAME, 'progress')))
 
-    jewel_table = driver.find_element(
-        By.XPATH, "/html/body/div/div[2]/div[1]/div[1]/div/div/table")
-    jewel_table_rows = jewel_table.find_elements(By.TAG_NAME, "tr")
-    for jewel_table_row in jewel_table_rows:
-        jewel_table_columns = jewel_table_row.find_elements(By.TAG_NAME, "td")
-        for jewel_table_column in jewel_table_columns:
-            print(jewel_table_column.text)
-
+    get_table_data(
+        driver, f"/html/body/div/div[2]/div[1]/div[1]/div/div/table")
     # channel = client.get_channel(900841296346886144)
     # await channel.send(file=jewel_table)
     driver.close()
---
+++ selenium_modules.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+
+
+def get_table_data(driver, table_xpath):
+    """Parameters - driver (object: selenium driver), table_xpath (string: xpath to table
+    on webpage)
+    Returns table in a double string list format
+    """
+
+    # Finds all rows in the table to iterate through
+    table = driver.find_element(
+        By.XPATH, table_xpath)
+    table_rows = table.find_elements(By.TAG_NAME, "tr")
+
+    # Initializes final table
+    new_table = []
+
+    # Iterates through all table cells
+    for table_row in table_rows:
+        table_columns = table_row.find_elements(By.TAG_NAME, "td")
+        new_row = [item.text for item in table_columns]
+        if (new_row):
+            new_table.append(new_row)
+
+    print(new_table)
+    return
